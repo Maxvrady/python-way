3f942542db2b7a20ca3f655778ffd08c540f6fdb
Max Vrady <MaxVrady@gmail.com>
2017-03-17 19:39:49 +0300

    Add comments

--- python_way.py
+++ python_way.py
@@ -5,24 +5,36 @@ from PyQt5.QtGui import QIcon, QFont
 
 class MyWindow(QWidget):
     def __init__(self):
+        # Creating app
         self.app = QApplication(sys.argv)
         super().__init__()
-        self.set_ui()
+        # Start general window
+        self.start_window()
+        # Run application
         self.app.exec_()
 
-    def set_ui(self):
+    def start_window(self):
+        # Creating font
         QToolTip.setFont(QFont('SansSerif', 10))
+        # Prompt for widget
         self.setToolTip("This is widget")
-
+        # Creating button
         self.button = QPushButton('Button', self)
+        # Creating prompt for button
         self.button.setToolTip("Its button")
+        # Button size
         self.button.resize(50, 50)
+        # Button position
         self.button.move(30, 30)
 
+        # Set window
         self.setGeometry(300, 300, 1400, 900)
+        # Set win title
         self.setWindowTitle("Windows")
+        # Set icon
         self.setWindowIcon(QIcon('picture/Samurai_smal.png'))
 
+        # Show window
         self.show()
 
 
